2c17f4f1641733a5add4c93a9527756e01df79bd
drfox81 <drfox11181@mail.ru>
2022-09-20 23:54:20 +0300

    сложение

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        double dog = 6;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        System.out.println(paper);
+        paper = paper + 4;
+        System.out.println(paper);
+        paper = paper - 7639;
+        System.out.println(paper);
+    }
+}
