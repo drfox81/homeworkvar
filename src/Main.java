64572ba330e267e20ee2d28c1c54793a5abd9d8a
drfox81 <drfox11181@mail.ru>
2022-09-22 20:38:38 +0300

    задание 1 задачи 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,20 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
         dog = dog + 4;
         System.out.println(dog);
-        dog = dog - 3.5;
-        System.out.println(dog);
-        System.out.println(cat);
         cat = cat + 4;
         System.out.println(cat);
-        cat = cat - 1.6;
-        System.out.println(cat);
-        System.out.println(paper);
         paper = paper + 4;
         System.out.println(paper);
+
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
         paper = paper - 7639;
         System.out.println(paper);
     }
